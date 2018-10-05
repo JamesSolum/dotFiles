c50f7fb986193ed67629f53ece0a7151db0cba1b
James Solum <james.solum@procore.com>
2018-10-04 21:28:50 -0700

    linux setup

---
+++ scripts/setupLinux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+############################################################
+#
+# Description:  Generic Linux Terminal Configuration Script
+# Author:       James Solum
+#
+############################################################
+
+# Setup Vimrc
+ln -s ~/git/dotFiles/vimrc ~/.vimrc
+
+# Setup Git Config 
+ln -s ~/git/dotFiles/gitconfig  ~/.gitconfig
+
+# Setup Tmux Config
+ln -s ~/git/dotFiles/tmux.conf ~/.tmux.conf
+
+# Setup Bashrc
+ln -s ~/git/dotFiles/bashrc ~/.bashrc
+
+# Setup System variables
+ln -s ~/git/dotFiles/system_variables/work_vars ~/.custom_system_vars
+
+# Setup Generic Linux Profile
+ln -s ~/git/dotFiles/custom_bash_configs/linux_generic_profile ~/.bash_profile
+
+# Setup
+ln -s ~/git/dotFiles/custom_bash_configs/personal_profile ~/.custom_bash_config
